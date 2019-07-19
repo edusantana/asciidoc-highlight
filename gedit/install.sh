252cb248d0e6e1b785c6eeb80fdf3119e92d220f
Jörg Eichhorn <joerg.eichhorn@kiwigrid.com>
2019-07-19 11:16:04 +0200

    Add gtksourceview-4 to install/uninstall scripts

--- gedit/install.sh
+++ gedit/install.sh
@@ -6,3 +6,6 @@ ln -s $SRC/asciidoc.lang
 mkdir -p ~/.local/share/gtksourceview-2.0/language-specs/
 cd ~/.local/share/gtksourceview-2.0/language-specs/
 ln -s $SRC/asciidoc.lang
+mkdir -p ~/.local/share/gtksourceview-4/language-specs/
+cd ~/.local/share/gtksourceview-4/language-specs/
+ln -s $SRC/asciidoc.lang
--- gedit/uninstall.sh
+++ gedit/uninstall.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 rm ~/.local/share/gtksourceview-3.0/language-specs/asciidoc.lang
 rm ~/.local/share/gtksourceview-2.0/language-specs/asciidoc.lang
+rm ~/.local/share/gtksourceview-4/language-specs/asciidoc.lang
